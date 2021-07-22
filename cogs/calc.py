e0c2e896b588973a17eef794a59aa7d11a8c7939
AayanTDE <aayantde@hotmail.com>
2021-07-22 17:23:59 +0500

    added how many bosses to csl

--- cogs/calc.py
+++ cogs/calc.py
@@ -44,7 +44,8 @@ class Calc(commands.Cog):
 
     @commands.command(aliases=["csl"])
     async def calcslayer(self, ctx, start, end, type):
-        required = "{:,}".format(slayerdiff(start, end, type))
+        required = slayerdiff(start, end, type)
+        required_str = "{:,}".format(required)
 
         if type in ["zombie", "revenant", "rev", "r"]:
             type = "Revenant"
@@ -52,11 +53,16 @@ class Calc(commands.Cog):
             type = "Tarantula"
         elif type in ["wolf", "sven", "s", "enderman", "eman", "e", "voidbloom", "v"]:
             type = "Sven/Enderman"
- 
+
         embed = Embed(
-            description=f"{required} xp is required to get from {type} {start} to {end}.",
+            description=f"{required_str} xp is required to get from {type} {start} to {end}.",
             colour=ctx.guild.me.color,
         )
+        embed.add_field(name="T1", value=ceil(required / 5), inline=False)
+        embed.add_field(name="T2", value=ceil(required / 25), inline=False)
+        embed.add_field(name="T3", value=ceil(required / 100), inline=False)
+        embed.add_field(name="T4", value=ceil(required / 500), inline=False)
+        embed.add_field(name="T5", value=ceil(required / 1500), inline=False)
         embed.set_footer(
             text="Made by yan#0069",
             icon_url="https://cdn.discordapp.com/avatars/270141848000004097/a_6022d1ac0f1f2b9f9506f0eb06f6eaf0.gif",

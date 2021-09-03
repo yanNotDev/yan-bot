ce156b319e532a621eb79c50719c1124a158874c
AayanTDE <aayantde@hotmail.com>
2021-09-03 13:07:27 +0500

    never die

--- util/lbin/lowestBIN.py
+++ util/lbin/lowestBIN.py
@@ -10,10 +10,14 @@ def update_json():
     with open("util/lbin/lowestbin.json", "w") as file:
         dump(data, file, indent=4)
 
+
 while True:
-    t1 = perf_counter()
-    update_json()
-    t2 = perf_counter()
-    duration = t2 - t1
-    print(f"Updated lowest BIN cache in {duration} seconds")
-    sleep(120 - duration)
+    try:
+        t1 = perf_counter()
+        update_json()
+        t2 = perf_counter()
+        duration = t2 - t1
+        print(f"Updated lowest BIN cache in {duration} seconds")
+        sleep(120 - duration)
+    except:
+        pass
